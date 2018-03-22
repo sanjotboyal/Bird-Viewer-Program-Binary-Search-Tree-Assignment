c8c4b2d672b4bf761b34061c95aa39023dd52dbd
Sanjot Boyal <sboyal2@uwo.ca>
2018-03-22 18:05:51 -0400

    Initial commit

---
+++ Birds/src/birds/BirdsController.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package birds;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.MenuBar;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author Abdelkader
+ */
+public class BirdsController implements Initializable {
+
+    @FXML
+    private MenuBar mainMenu;
+    
+
+    @FXML
+    public void exit() {
+        Stage stage = (Stage) mainMenu.getScene().getWindow();
+        stage.close();
+    }
+    
+    public void fillDictionary(){
+        
+    }
+    
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        
+    }
+
+}
---
+++ Birds/src/birds/DataKey.java
@@ -0,0 +1,21 @@
+package birds;
+
+public class DataKey {
+
+    
+	// default constructor
+	public DataKey() {
+	 
+	}
+        
+	// other constructors
+
+	/**
+	 * Returns 0 if this DataKey is equal to k, returns -1 if this DataKey is smaller
+	 * than k, and it returns 1 otherwise. 
+	 */
+	public int compareTo(DataKey k) {
+           return 0;
+            
+	}
+}
